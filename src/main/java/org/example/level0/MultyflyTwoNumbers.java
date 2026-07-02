d2cd6fea30b4dcdb2fb38ab8a601258e5db5d698
wonjonghwan <woneric05@gmail.com>
2026-07-02 16:39:06 +0900

    두 수의 곱

--- src/main/java/org/example/level0/MultyflyTwoNumbers.java
+++ src/main/java/org/example/level0/MultyflyTwoNumbers.java
@@ -1,9 +1,27 @@
 package org.example.level0;
 public class MultyflyTwoNumbers {
-public int solution(int num1, int num2) {
-    int answer = 0;
-    answer=num1 * num2;
-    return answer;
+    public static void main(String[] args) {
+        /*
+        정수 num1, num2가 매개변수 주어집니다.
+        num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+         */
+        int num1 = 27;
+        int num2 = 19;
+        System.out.println(soulution(num1, num2));
+    }
 
-}
+    static int soulution(int num1, int num2) {
+
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+
+    }
+
+
+
+
+
+
+
 }
