33dacbba8174290d393a6168328c5714ab8f1e23
wonjonghwan <woneric05@gmail.com>
2026-07-07 20:35:28 +0900

    나이 구하기

---
+++ src/main/java/org/example/level0/AgePrint.java
@@ -0,0 +1,24 @@
+package org.example.level0;
+
+public class AgePrint {
+    public static void main(String[] args) {
+
+        /*머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 2022년 기준 선생님의 나이 age가 주어질 때,
+        선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+         */
+        int age = 40;
+
+        System.out.println(solution( age));
+
+    }
+    static int solution(int age){
+        int answer = 0;
+
+        if(age > 120 || age < 0){
+            System.out.println(".잘못된 나이 입력");
+           return -1;
+        }
+        answer = 2022 - age + 1;
+        return answer;
+    }
+}
